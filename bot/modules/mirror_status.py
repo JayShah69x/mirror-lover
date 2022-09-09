21a32c778018063d9d1e128bde549b91cdeb40d4
woodcraft5 <sekhfaruk5@gmail.com>
2022-09-10 02:32:26 +0530

    love-code❤️

--- bot/modules/mirror_status.py
+++ bot/modules/mirror_status.py
@@ -16,8 +16,8 @@ def mirror_status(update, context):
     if count == 0:
         currentTime = get_readable_time(time() - botStartTime)
         free = get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)
-        message = '✧No Active Downloads !\n✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧'
-        message += f"\n<b>⇛  CPU •</b> {cpu_percent()}% | <b>⇛ FREE •</b> {free}" \
+        message = '⇛ Baby No Active Downloads !\n⇛ ✧✦✧✦✧✦✧✦✧✦✧✦✧✦✧'
+        message += f"\n<b>⇛ CPU •</b> {cpu_percent()}% | <b>⇛ FREE •</b> {free}" \
                    f"\n<b>⇛ RAM •</b> {virtual_memory().percent}% | <b>⇛ UPTIME •</b> {currentTime}"
         reply_message = sendMessage(message, context.bot, update.message)
         Thread(target=auto_delete_message, args=(context.bot, update.message, reply_message)).start()
